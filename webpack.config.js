b656c6196745e72ff2094c05113cbd578b02ca0f
alejleon <aherna4@gmail.com>
2021-10-31 03:04:45 -0500

    restructuring in progress

--- webpack.config.js
+++ webpack.config.js
@@ -4,54 +4,58 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
 
-  entry: '/home/al/linux/webpackBoiler/index.js',
+  entry: path.resolve(__dirname, './src/index.js'),
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     publicPath: '',
     filename: 'bundle.js'
   },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(sa|sc|s)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "postcss-loader",
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              implementation: require("sass")
-            }
-          }
-        ]
-      }
-    ]
+  devServer: {
+    static: path.resolve(__dirname, './public'),
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "bundle.css"
-    }),
-    new htmlPlugin({template: './public/index.html'})
-  ],
 
-  mode: 'development'
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /(node_modules)/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['@babel/preset-env']
+  //         }
+  //       }
+  //     },
+  //     {
+  //       test: /\.(sa|sc|s)ss$/,
+  //       use: [
+  //         {
+  //           loader: MiniCssExtractPlugin.loader
+  //         },
+  //         {
+  //           loader: "css-loader"
+  //         },
+  //         {
+  //           loader: "postcss-loader",
+  //         },
+  //         {
+  //           loader: "sass-loader",
+  //           options: {
+  //             implementation: require("sass")
+  //           }
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // },
+  // plugins: [
+  //   new MiniCssExtractPlugin({
+  //     filename: "bundle.css"
+  //   }),
+  //   new htmlPlugin({template: './public/index.html'})
+  // ],
+
+  // mode: 'development'
 }
