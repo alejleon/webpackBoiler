b041ecd1713b76f5bcdeb520f4df2a4542739c1f
alejleon <aherna4@gmail.com>
2021-11-01 02:21:33 -0500

    babel with react setup complete

---
+++ components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  console.log('Yooooo! It\'s working!')
+
+  return(
+    <div>React Application</div>
+  )
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,22 @@ module.exports = {
 
   entry: path.resolve(__dirname, './src/index.js'),
 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ],
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
   output: {
     path: path.resolve(__dirname, 'public'),
-    publicPath: '',
     filename: 'bundle.js'
   },
 
